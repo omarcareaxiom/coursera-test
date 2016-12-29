22cb9bde430fb0b9ee88c51b899869462cd5a5a8
Omar Iqbal Naru <omar.iqbal@careaxiom.com>
2016-12-29 15:16:06 +0500

    initial commit

---
+++ module2-solution/app.js
@@ -0,0 +1,16 @@
+(function(){
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController);
+
+ToBuyController.$inject = ['$scope'];
+function ToBuyController($scope){
+}
+
+AlreadyBoughtController.$inject = ['$scope'];
+function AlreadyBoughtController($scope){
+}
+
+})();
